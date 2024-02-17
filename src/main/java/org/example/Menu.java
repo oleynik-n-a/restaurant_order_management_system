68821d48ee30c419da632ab7facb6e8fa9149433
oleynik-n-a <naoleynik4@gmail.com>
2024-02-17 22:53:22 +0300

    Fix for class menu

--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -7,13 +7,18 @@ import java.util.List;
 
 public class Menu {
     private final List<Dish> dishesList;
+    private final Serializer<List<Dish>> serializer;
 
     public Menu() throws IOException, ClassNotFoundException {
-        Serializer<List<Dish>> serializer = new Serializer<List<Dish>>("../Database/");
+        serializer = new Serializer<List<Dish>>("../Database/");
         dishesList = serializer.Deserialize();
     }
 
     public List<Dish> getDishesList() {
         return dishesList;
+    }
+
+    public void SaveData() throws IOException, ClassNotFoundException {
+        serializer.Serialize(dishesList);
     }
 }
