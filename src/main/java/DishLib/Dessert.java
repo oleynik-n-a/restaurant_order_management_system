017c68bd12cd9f84168f059a224881989597857f
oleynik-n-a <naoleynik4@gmail.com>
2024-02-23 17:30:47 +0300

    Added some logic to class Admin and some another classes

--- src/main/java/DishLib/Dessert.java
+++ src/main/java/DishLib/Dessert.java
@@ -4,4 +4,9 @@ public class Dessert extends Dish {
     public Dessert(String name, int cost, int cookingTime) {
         super(name, cost, cookingTime);
     }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", type: dessert.";
+    }
 }
--- src/main/java/DishLib/DishBuilder.java
+++ src/main/java/DishLib/DishBuilder.java
@@ -8,14 +8,29 @@ public abstract class DishBuilder implements IBuilder<Dish> {
     protected int _cookingTime;
 
     public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name can't be null or empty.");
+        }
         _name = name;
     }
 
     public void setCost(int cost) {
+        if (cost <= 0) {
+            throw new IllegalArgumentException("Cost can't be negative or zero.");
+        }
         _cost = cost;
     }
 
     public void setCookingTime(int cookingTime) {
+        if (cookingTime <= 0) {
+            throw new IllegalArgumentException("Cooking time can't be negative or zero.");
+        }
+        if (cookingTime < 5) {
+            throw new IllegalArgumentException("Too low. Cooking time should be at least 5 minutes.");
+        }
+        if (cookingTime > 40) {
+            throw new IllegalArgumentException("Too high. Cooking time should be up to 40 minutes.");
+        }
         _cookingTime = cookingTime;
     }
 
